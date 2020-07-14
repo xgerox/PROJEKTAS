e46fb3524f8736342eca4d5c598eb85f8fd9ffc9
xgerox <32131195+xgerox@users.noreply.github.com>
2020-07-15 01:13:18 +0300

    Create test.php
    Aha

---
+++ PROJEKTAS/test.php
@@ -0,0 +1,94 @@
+
+<link href='https://fonts.googleapis.com/css?family=Varela' rel='stylesheet' type='text/css'>
+
+<div class="glitch" data-text="SKILLS">SKILLS</div> 
+
+
+
+<div class="skills">
+  
+  <p><h2> Design </h2></p>
+    
+       
+    <img width="50px" height="50px" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Adobe_Photoshop_CC_icon.svg/66px-Adobe_Photoshop_CC_icon.svg.png">&nbsp; Photoshop</img></span>
+
+<span class="bar">
+      <span class="photoshop"></span></span>
+
+
+    <img width="50px" height="50px" src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Adobe_Illustrator_CC_icon.svg/1051px-Adobe_Illustrator_CC_icon.svg.png">&nbsp; Illustrator</img></span>
+
+<span class="bar">
+      <span class="illustrator"></span></span>
+
+
+
+    <img width="50px" height="50px" src="https://seeklogo.com/images/A/adobe-lightroom-logo-6A4DC55983-seeklogo.com.png">&nbsp; Lightroom</img></span>
+
+<span class="bar">
+      <span class="lightroom"></span></span>
+
+
+
+<img width="50px" height="50px" src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/39/Vegas_Pro_15.0.png/600px-Vegas_Pro_15.0.png">&nbsp; VegasPro</img>
+
+<span class="bar">
+      <span class="vegaspro"></span></span>
+
+<p><h2> Programming </h2></p>
+
+
+<img width="50px" height="50px" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/120px-HTML5_logo_and_wordmark.svg.png">&nbsp; Html5</img>
+
+<span class="bar">
+      <span class="html5"></span></span>
+
+
+<img width="50px" height="50px" src="https://pluralsight.imgix.net/paths/path-icons/css-c9b214f0d7.png">&nbsp; CSS</img>
+
+<span class="bar">
+      <span class="css"></span></span>
+
+
+
+<img width="50px" height="50px" src="https://upload.wikimedia.org/wikipedia/en/thumb/6/62/MySQL.svg/1200px-MySQL.svg.png">&nbsp; MySql</img>
+
+<span class="bar">
+      <span class="mysql"></span></span>
+
+
+
+
+<img width="50px" height="50px" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/PHP-logo.svg/1200px-PHP-logo.svg.png">&nbsp; Php</img>
+
+<span class="bar">
+      <span class="php"></span></span>
+
+
+
+  <p><h2> Electrotechnology </h2></p>
+
+
+
+<img style="border-radius: 10px;" width="50px" height="50px" src="https://w7.pngwing.com/pngs/759/217/png-transparent-square-black-logo-integrated-circuits-chips-central-processing-unit-computer-icons-chip-electronics-text-computer.png">&nbsp; Computer engineering</img>
+
+<span class="bar">
+      <span class="computer"></span></span>
+
+
+
+
+<img width="50px" height="50px" src="https://seeklogo.com/images/W/windows-10-icon-logo-5BC5C69712-seeklogo.com.png">&nbsp; Windows</img>
+
+<span class="bar">
+      <span class="windows"></span></span>
+
+<img width="50px" height="50px" src="https://cdn.worldvectorlogo.com/logos/android.svg">&nbsp; Android</img>
+
+<span class="bar">
+      <span class="android"></span></span>
+
+<img width="50px" height="50px" src="https://i0.wp.com/www.linuxandubuntu.com/wp-content/uploads/2019/07/linux-logo-penguin.png">&nbsp; Linux</img>
+
+<span class="bar">
+      <span class="linux"></span></span>
